283491f0ac333976766931b2cf01312d0f8cfcbd
maram <maram.ktaha@gmail.com>
2024-07-09 16:19:28 +0300

    update monthfield serializer

--- month/serializers.py
+++ month/serializers.py
@@ -1,21 +1,29 @@
 import datetime
 from rest_framework import fields, serializers
 
-from month import Month, models
+from month import Month
 
 class MonthField(fields.DateField):
-  
+
   def to_internal_value(self, value):
-    if isinstance(value, Month):
-        month = value
-    elif isinstance(value, datetime.date):
-        month = Month.from_date(value)
-        if len(str(month.year)) < 4:
-            raise serializers.ValidationError(
-                self.error_messages['invalid_year'],
-                code='invalid_year',
-                params={'value': value},
-            )
-    
+        if isinstance(value, Month):
+            month = value
+        elif isinstance(value, datetime.date) or isinstance(value,str):
+            print(value,type(value))
+
+            if isinstance(value,str):
+                month = Month.from_string(value)
+            else:
+                month = Month.from_date(value)
+            print(str(month.year))
+            if len(str(month.year)) < 4:
+                raise serializers.ValidationError(
+                    self.error_messages["invalid_year"],
+                    code="invalid_year",
+                    params={"value": value},
+                )
+        return month
+
   def to_representation(self, value):
-    return value.strftime(value)
+
+        return value.strftime(str(value))
